44a8c51a37637251faffa9b34c1166e586570fa8
Auli Tallberg <auli.tallberg@gmail.com>
2020-11-06 11:00:10 +0200

    3.3 done

--- index.js
+++ index.js
@@ -45,6 +45,19 @@ app.get('/api/info', (req, res) => {
         <p> ${new Date()}</p >`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        console.log("success")
+        response.json(person)
+    } else {
+        console.log("failed")
+        response.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
